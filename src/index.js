22a07054410df99f9e839d11ec8fdf4346fd59bd
MHatalyak <matronahu@gmail.com>
2023-06-13 18:28:17 +0300

    added files

---
+++ src/index.js
@@ -0,0 +1,113 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let currentPage = 1;
+let searchQuery = '';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  searchQuery = e.target.searchQuery.value.trim();
+
+  if (searchQuery === '') {
+    Notify.failure('Please enter a search query');
+    return;
+  }
+
+  currentPage = 1;
+  gallery.innerHTML = '';
+  loadMoreBtn.style.display = 'none';
+
+  searchImages();
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  currentPage++;
+  searchImages();
+});
+
+async function searchImages() {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '30554094-a84a8756902b4f7be7a5ac4d7',
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: currentPage,
+        per_page: 40,
+      },
+    });
+
+    const { data } = response;
+
+    if (data.hits.length === 0) {
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    const totalHits = data.totalHits || 0;
+    const images = data.hits;
+
+    images.forEach(image => {
+      const card = createImageCard(image);
+      gallery.appendChild(card);
+    });
+
+    if (totalHits > currentPage * 40) {
+      loadMoreBtn.style.display = 'block';
+    } else {
+      loadMoreBtn.style.display = 'none';
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    Notify.failure(
+      'An error occurred while fetching images. Please try again later.'
+    );
+    console.error(error);
+  }
+}
+
+function createImageCard(image) {
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
+
+  const img = document.createElement('img');
+  img.src = image.webformatURL;
+  img.alt = image.tags;
+  img.loading = 'lazy';
+
+  const info = document.createElement('div');
+  info.classList.add('info');
+
+  const likes = document.createElement('p');
+  likes.classList.add('info-item');
+  likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
+
+  const views = document.createElement('p');
+  views.classList.add('info-item');
+  views.innerHTML = `<b>Views:</b> ${image.views}`;
+
+  const comments = document.createElement('p');
+  comments.classList.add('info-item');
+  comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
+
+  const downloads = document.createElement('p');
+  downloads.classList.add('info-item');
+  downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
+
+  info.appendChild(likes);
+  info.appendChild(views);
+  info.appendChild(comments);
+  info.appendChild(downloads);
+
+  card.appendChild(img);
+  card.appendChild(info);
+
+  return card;
+}
